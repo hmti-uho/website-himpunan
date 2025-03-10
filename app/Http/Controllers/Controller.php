c5d445f6609397310ac5db62a9b2136c29498a4e
Mansadhit301 <aadhit407@gmail.com>
2025-03-10 14:40:09 +0800

    feat: create CRUD operation for Event using filament

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+
 abstract class Controller
 {
-    //
+    public function index()
+    {
+        // Ambil semua data events dari database
+        $events = Event::all();
+
+        // Kirim data ke view events.blade.php
+        return view("events", compact("events"));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // Cari event berdasarkan ID
+        $event = Event::findOrFail($id);
+
+        // Kirim data ke view event_show.blade.php
+        return view("event_show", compact("event"));
+    }
 }

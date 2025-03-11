96dbfd5595167e64853de407241b046c45d4b5f7
Mansadhit301 <aadhit407@gmail.com>
2025-03-11 20:39:17 +0800

    feat: create function index and show in controller event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index() {
+        $events = Event::all();
+
+        return view("events", compact($events));
+    }
+
+    public function show($id) {
+        $event = Event::findOrFail($id);
+
+        return view("event_show", compact($event));
+    }
 }
